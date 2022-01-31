10a5b41d2e7dcd2286206310fcf264f8ac202313
KecoaBerotak <dennisfernandes0212@gmail.com>
2022-01-31 16:32:18 +0700

    tambah edit data

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,31 @@
 import React from "react";
 import SearchInput from "./SearchInput";
 import axios from "axios";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
   constructor(props){
     super(props)
 
+    this.state = {images: [] }
+
     this.onSearchSubmit = this.onSearchSubmit.bind(this)
   }
 
-  onSearchSubmit(entry){
-    console.log(entry)
-    axios.get(`https://pixabay.com/api/?key=25489413-2c7cd31196f9d8f827a106d2e&q=${entry}&image_type=photo`)
+  async onSearchSubmit(entry){
+    const response = await axios.get(`https://pixabay.com/api/?key=25489413-2c7cd31196f9d8f827a106d2e&q=${entry}&image_type=photo`)
+    this.setState({images: response.data.hits})
   }
 
   render(){
     return (
       <div className="ui container" style={{marginTop: '30px'}}>
         <SearchInput onSearchSubmit = {this.onSearchSubmit} />
+        We have {this.state.images.length} images
+        <br />
+        <ImageList images = {this.state.images}/>
       </div>
-    )    
+    )
   }
 }
 
