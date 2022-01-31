6153040ebc78af0481279cf034eac21c3aa0336c
KecoaBerotak <dennisfernandes0212@gmail.com>
2022-01-31 13:55:49 +0700

    edit index.js & menambahkan folder components

---
+++ src/components/SearchInput.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class SearchInput extends React.Component {
+    render() {
+        return (
+            <div>
+                <form action="">
+                    <input type="Text" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchInput;
