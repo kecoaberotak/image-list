f13c8a55cbe11306df4f0b0c1f7f67f63b874bae
KecoaBerotak <dennisfernandes0212@gmail.com>
2022-01-31 15:33:29 +0700

    edit App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import SearchInput from "./SearchInput";
 
 const App = () => {
-    return (
-        <div className="ui container" style={{marginTop: '30px'}}>
-            <SearchInput/>
-        </div>
-    )
+  return (
+    <div className="ui container" style={{marginTop: '30px'}}>
+      <SearchInput/>
+    </div>
+  )
 }
 
 export default App;
